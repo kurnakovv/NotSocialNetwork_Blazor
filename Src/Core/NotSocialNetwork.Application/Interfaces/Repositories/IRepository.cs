0c55ec3e6fd14a8e1b57d999305c1aef960186e2
KurnakovMaksim <maksk210@gmail.com>
2021-02-25 18:28:38 +0900

    Add repository with implementation EFCore

---
+++ Src/Core/NotSocialNetwork.Application/Interfaces/Repositories/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NotSocialNetwork.Application.Interfaces.Repositories
+{
+    public interface IRepository<T>
+    {
+        IEnumerable<T> GetAll();
+        T Get(Guid id);
+        T Add(T t);
+        T Update(T t);
+        T Delete(Guid id);
+        void Commit();
+    }
+}
---
+++ Src/Infrastructure/NotSocialNetwork.Data/EFRepositories/EFCoreRepository.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NotSocialNetwork.Application.Entities.Abstract;
+using NotSocialNetwork.Application.Interfaces.Repositories;
+using NotSocialNetwork.DBContexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NotSocialNetwork.Data.EFRepositories
+{
+    public class EFCoreRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        public EFCoreRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+            _dbSet = appDbContext.Set<T>();
+        }
+
+        private readonly AppDbContext _appDbContext;
+        private readonly DbSet<T> _dbSet;
+
+        public T Add(T t)
+        {
+            _dbSet.Add(t);
+            return t;
+        }
+
+        public T Get(Guid id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public T Update(T t)
+        {
+            _dbSet.Attach(t);
+            _dbSet.Update(t);
+            return t;
+        }
+
+        public T Delete(Guid id)
+        {
+            var t = Get(id);
+
+            if(_appDbContext.Entry(t).State == EntityState.Detached)
+            {
+                _dbSet.Attach(t);
+            }
+
+            _dbSet.Remove(t);
+
+            return t;
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _dbSet.ToList();
+        }
+
+        public void Commit()
+        {
+            _appDbContext.SaveChanges();
+        }
+    }
+}
