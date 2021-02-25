fa8247cc47a1b447cad49fdaddb5cf44471ddc6e
KurnakovMaksim <maksk210@gmail.com>
2021-02-25 18:17:54 +0900

    Add db context with setup for startup

---
+++ Src/Infrastructure/NotSocialNetwork.DBContexts/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NotSocialNetwork.Application.Entities;
+
+namespace NotSocialNetwork.DBContexts
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<UserEntity> Users { get; set; }
+        public DbSet<PublicationEntity> Publications { get; set; }
+    }
+}
---
+++ Src/Infrastructure/NotSocialNetwork.DBContexts/StartupSetup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace NotSocialNetwork.DBContexts
+{
+    public static class StartupSetup
+    {
+        public static void AddDbContext(this IServiceCollection services, string connectionString) =>
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(connectionString));
+    }
+}
