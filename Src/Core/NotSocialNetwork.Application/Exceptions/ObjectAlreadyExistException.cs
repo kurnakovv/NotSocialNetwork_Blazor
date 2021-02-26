b5afd1bf0174573590bb918aa8ef3026b96a333c
KurnakovMaksim <maksk210@gmail.com>
2021-02-26 17:40:33 +0900

    Add exceptions related with objects
    ObjectAlreadyExistException, ObjectNotFoundException

---
+++ Src/Core/NotSocialNetwork.Application/Exceptions/ObjectAlreadyExistException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace NotSocialNetwork.Application.Exceptions
+{
+    public class ObjectAlreadyExistException : InvalidOperationException
+    {
+        public ObjectAlreadyExistException(string message) : base(message) { }
+    }
+}
---
+++ Src/Core/NotSocialNetwork.Application/Exceptions/ObjectNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace NotSocialNetwork.Application.Exceptions
+{
+    public class ObjectNotFoundException : InvalidOperationException
+    {
+        public ObjectNotFoundException(string message) : base(message) { }
+    }
+}
