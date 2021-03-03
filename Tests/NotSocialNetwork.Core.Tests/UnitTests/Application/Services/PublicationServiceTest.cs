d5e3cbb5b0adf81bce0bc6f725af07de24c60ffe
KurnakovMaksim <maksk210@gmail.com>
2021-03-03 15:31:57 +0900

    Add unit test for IPublicationService

---
+++ Tests/NotSocialNetwork.Core.Tests/UnitTests/Application/Services/PublicationServiceTest.cs
@@ -0,0 +1,115 @@
+﻿using Moq;
+using NotSocialNetwork.Application.Entities;
+using NotSocialNetwork.Application.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace NotSocialNetwork.Core.Tests.UnitTests.Application.Services
+{
+    public class PublicationServiceTest
+    {
+        private readonly IEnumerable<PublicationEntity> _publications = new List<PublicationEntity>()
+        {
+            new PublicationEntity()
+            {
+                Title = "Title1",
+                Author = new UserEntity()
+                {
+                    Name = "Name1",
+                    DateOfBirth = DateTime.Now,
+                    Email = "firstEmail@gmail.com",
+                },
+                Images = new List<string>(){ "img1.jpg", "img2.jpg"},
+            },
+            new PublicationEntity()
+            {
+                Title = "Title2",
+                Author = new UserEntity()
+                {
+                    Name = "Name1",
+                    DateOfBirth = DateTime.Now,
+                    Email = "firstEmail@gmail.com",
+                },
+                Images = new List<string>(){ "img1.jpg", "img2.jpg"},
+            }
+        };
+
+        private readonly PublicationEntity _publication = new PublicationEntity()
+        {
+            Title = "Title",
+            Images = new List<string>() { "img.jpg", "img.png" },
+            Author = new UserEntity() { Name = "Name", DateOfBirth = DateTime.Now, Email = "some@gmail.com" },
+        };
+
+        [Fact]
+        public void GetAll_GetAllPublications_Publications()
+        {
+            // Arrange
+            var publicationService = new Mock<IPublicationService>();
+
+            // Act
+            var result = publicationService.Setup(p => p.GetAll()).Returns(_publications);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(_publications, publicationService.Object.GetAll());
+        }
+
+        [Fact]
+        public void Add_AddPublication_Publication()
+        {
+            // Arrange
+            var publicationService = new Mock<IPublicationService>();
+
+            // Act
+            var result = publicationService.Setup(p => p.Add(_publication)).Returns(_publication);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(_publication, publicationService.Object.Add(_publication));
+        }
+
+        [Fact]
+        public void Delete_DeletePublication_Publication()
+        {
+            // Arrange
+            var publicationService = new Mock<IPublicationService>();
+
+            // Act
+            var result = publicationService.Setup(p => p.Delete(_publication.Id)).Returns(_publication);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(_publication, publicationService.Object.Delete(_publication.Id));
+        }
+
+        [Fact]
+        public void Get_GetPublication_Publication()
+        {
+            // Arrange
+            var publicationService = new Mock<IPublicationService>();
+
+            // Act
+            var result = publicationService.Setup(p => p.GetById(_publication.Id)).Returns(_publication);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(_publication, publicationService.Object.GetById(_publication.Id));
+        }
+
+        [Fact]
+        public void Update_UpdatePublication_Publication()
+        {
+            // Arrange
+            var publicationService = new Mock<IPublicationService>();
+
+            // Act
+            var result = publicationService.Setup(p => p.Update(_publication)).Returns(_publication);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(_publication, publicationService.Object.Update(_publication));
+        }
+    }
+}
