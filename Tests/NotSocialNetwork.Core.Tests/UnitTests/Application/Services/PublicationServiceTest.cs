d05d7caad7f45f65557a4a5201345518d8f0c1e3
KurnakovMaksim <maksk210@gmail.com>
2021-04-01 18:23:26 +0900

    Fix moq objects work in PublicationServiceTest

--- Tests/NotSocialNetwork.Core.Tests/UnitTests/Application/Services/PublicationServiceTest.cs
+++ Tests/NotSocialNetwork.Core.Tests/UnitTests/Application/Services/PublicationServiceTest.cs
@@ -1,8 +1,10 @@
 ﻿using Moq;
 using NotSocialNetwork.Application.Entities;
-using NotSocialNetwork.Application.Interfaces.Services;
+using NotSocialNetwork.Application.Interfaces.Repositories;
+using NotSocialNetwork.Application.Services;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace NotSocialNetwork.Core.Tests.UnitTests.Application.Services
@@ -46,70 +48,99 @@ namespace NotSocialNetwork.Core.Tests.UnitTests.Application.Services
         public void GetAll_GetAllPublications_Publications()
         {
             // Arrange
-            var publicationService = new Mock<IPublicationService>();
+            var publicationRepositoryMock = new Mock<IRepository<PublicationEntity>>();
+            var publicationService = new PublicationService(publicationRepositoryMock.Object);
+            
+            publicationRepositoryMock.Setup(r => r.GetAll())
+                                        .Returns(_publications.AsQueryable());
 
             // Act
-            var result = publicationService.Setup(p => p.GetAll()).Returns(_publications);
+            var result = publicationService.GetAll();
 
             // Assert
             Assert.NotNull(result);
-            Assert.Equal(_publications, publicationService.Object.GetAll());
+            Assert.Equal(_publications, result);
+            Assert.Equal("Title1", result.ElementAt(0).Title);
+            Assert.Equal("Title2", result.ElementAt(1).Title);
         }
 
         [Fact]
         public void Add_AddPublication_Publication()
         {
             // Arrange
-            var publicationService = new Mock<IPublicationService>();
+            var publicationRepositoryMock = new Mock<IRepository<PublicationEntity>>();
+            var publicationService = new PublicationService(publicationRepositoryMock.Object);
 
             // Act
-            var result = publicationService.Setup(p => p.Add(_publication)).Returns(_publication);
+            var result = publicationService.Add(_publication);
 
             // Assert
             Assert.NotNull(result);
-            Assert.Equal(_publication, publicationService.Object.Add(_publication));
+            Assert.Equal(_publication, result);
+            Assert.Equal(_publication.Id, result.Id);
+            Assert.Equal(_publication.Title, result.Title);
         }
 
         [Fact]
         public void Delete_DeletePublication_Publication()
         {
             // Arrange
-            var publicationService = new Mock<IPublicationService>();
+            var publicationRepositoryMock = new Mock<IRepository<PublicationEntity>>();
+            var publicationService = new PublicationService(publicationRepositoryMock.Object);
+
+            publicationRepositoryMock.Setup(r => r.Get(_publication.Id))
+                                        .Returns(_publication);
 
             // Act
-            var result = publicationService.Setup(p => p.Delete(_publication.Id)).Returns(_publication);
+            var result = publicationService.Delete(_publication.Id);
 
             // Assert
             Assert.NotNull(result);
-            Assert.Equal(_publication, publicationService.Object.Delete(_publication.Id));
+            Assert.Equal(_publication, result);
+            Assert.Equal(_publication.Id, result.Id);
+            Assert.DoesNotContain(_publication, publicationService.GetAll());
         }
 
         [Fact]
         public void Get_GetPublication_Publication()
         {
             // Arrange
-            var publicationService = new Mock<IPublicationService>();
+            var publicationRepositoryMock = new Mock<IRepository<PublicationEntity>>();
+            var publicationService = new PublicationService(publicationRepositoryMock.Object);
+
+            publicationRepositoryMock.Setup(r => r.Get(_publication.Id))
+                                        .Returns(_publication);
 
             // Act
-            var result = publicationService.Setup(p => p.GetById(_publication.Id)).Returns(_publication);
+            var result = publicationService.GetById(_publication.Id);
 
             // Assert
             Assert.NotNull(result);
-            Assert.Equal(_publication, publicationService.Object.GetById(_publication.Id));
+            Assert.Equal(_publication, result);
+            Assert.Equal(_publication.Id, result.Id);
+            Assert.Equal(_publication.Title, result.Title);
         }
 
         [Fact]
         public void Update_UpdatePublication_Publication()
         {
             // Arrange
-            var publicationService = new Mock<IPublicationService>();
+            var publicationRepositoryMock = new Mock<IRepository<PublicationEntity>>();
+            var publicationService = new PublicationService(publicationRepositoryMock.Object);
+
+            publicationRepositoryMock.Setup(r => r.Get(_publication.Id))
+                                        .Returns(_publication);
+
+            _publication.Title = "TestTitle";
 
             // Act
-            var result = publicationService.Setup(p => p.Update(_publication)).Returns(_publication);
+            var result = publicationService.Update(_publication);
 
             // Assert
             Assert.NotNull(result);
-            Assert.Equal(_publication, publicationService.Object.Update(_publication));
+            Assert.Equal(_publication, result);
+            Assert.Equal(_publication.Id, result.Id);
+            Assert.Equal(_publication.Title, result.Title);
         }
     }
 }
