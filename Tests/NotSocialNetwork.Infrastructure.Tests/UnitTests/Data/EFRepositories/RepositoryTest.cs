8d83a244ff3cd2c3cc0b5c0732c7aa9ffe247462
KurnakovMaksim <maksk210@gmail.com>
2021-04-02 16:58:11 +0900

    Delete moq from Repository unit test
    Moq -> MemoryDb

--- Tests/NotSocialNetwork.Infrastructure.Tests/UnitTests/Data/EFRepositories/RepositoryTest.cs
+++ Tests/NotSocialNetwork.Infrastructure.Tests/UnitTests/Data/EFRepositories/RepositoryTest.cs
@@ -4,26 +4,51 @@ using System;
 using System.Collections.Generic;
 using Xunit;
 using System.Linq;
+using NotSocialNetwork.DBContexts;
+using NotSocialNetwork.Data.EFRepositories;
+using NotSocialNetwork.Application.Entities.Abstract;
+using NotSocialNetwork.Application.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace NotSocialNetwork.Infrastructure.Tests.Data.EFRepositories
 {
-    public class RepositoryTest
+    public class RepositoryTest : IDisposable
     {
-        private readonly IEnumerable<MockObject> _objects = new List<MockObject>()
+        public RepositoryTest()
         {
-            new MockObject()
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase("TestDb")
+                .Options;
+
+            _appDbContext = new AppDbContext(options);
+
+            _appDbContext.Database.EnsureCreated();
+
+            Seed(_appDbContext);
+        }
+
+        private readonly AppDbContext _appDbContext;
+
+        private void Seed(AppDbContext appDbContext)
+        {
+            appDbContext.AddRange(_users);
+
+            appDbContext.SaveChanges();
+        }
+
+        private readonly IQueryable<UserEntity> _users = new List<UserEntity>()
+        {
+            new UserEntity()
             {
-                Id = Guid.NewGuid(),
                 Name = "Name1",
             },
-            new MockObject()
+            new UserEntity()
             {
-                Id = Guid.NewGuid(),
                 Name = "Name2",
             },
-        };
+        }.AsQueryable();
 
-        private readonly MockObject _object = new()
+        private readonly UserEntity _user = new UserEntity()
         {
             Id = Guid.NewGuid(),
             Name = "Name",
@@ -33,75 +58,93 @@ namespace NotSocialNetwork.Infrastructure.Tests.Data.EFRepositories
         public void GetAll_GetAllObjects_Objects()
         {
             // Arrange
-            var repository = new Mock<IRepository<MockObject>>();
+            var repository = new EFCoreRepository<UserEntity>(_appDbContext);
 
             // Act
-            var result = repository.Setup(r => r.GetAll()).Returns((MockObject)_objects.ToList());
+            var result = repository.GetAll();
 
             // Assert
-            Assert.Equal(_objects, repository.Object.GetAll());
+            Assert.NotNull(result);
+            Assert.Equal(_users, result);
+            Assert.Equal(2, result.Count());
+            Assert.Equal("Name2", result
+                                    .ToList()
+                                    .ElementAt(1).Name);
         }
 
         [Fact]
         public void Add_AddObject_Object()
         {
             // Arrange
-            var repository = new Mock<IRepository<MockObject>>();
+            var repository = new EFCoreRepository<UserEntity>(_appDbContext);
 
             // Act
-            var result = repository.Setup(u => u.Add(_object)).Returns(_object);
+            var result = repository.Add(_user);
+            repository.Commit();
 
             // Assert
             Assert.NotNull(result);
-            Assert.Equal(_object, repository.Object.Add(_object));
+            Assert.Equal(_user, result);
+            Assert.Equal("Name", result.Name);
         }
 
         [Fact]
         public void Delete_DeleteObject_Object()
         {
             // Arrange
-            var repository = new Mock<IRepository<MockObject>>();
+            var repository = new EFCoreRepository<UserEntity>(_appDbContext);
 
             // Act
-            var result = repository.Setup(u => u.Delete(_object.Id)).Returns(_object);
+            var result = repository.Delete(_users.First().Id);
+            repository.Commit();
 
             // Assert
             Assert.NotNull(result);
-            Assert.Equal(_object, repository.Object.Delete(_object.Id));
+            Assert.Equal(_users.First(), result);
         }
 
         [Fact]
         public void Get_GetObject_Object()
         {
             // Arrange
-            var repository = new Mock<IRepository<MockObject>>();
+            var repository = new EFCoreRepository<UserEntity>(_appDbContext);
 
             // Act
-            var result = repository.Setup(u => u.Get(_object.Id)).Returns(_object);
+            var result = repository.Get(_users.First().Id);
 
             // Assert
             Assert.NotNull(result);
-            Assert.Equal(_object, repository.Object.Get(_object.Id));
+            Assert.Equal("Name1", result.Name);
+            Assert.Equal(_users.First().Id, result.Id);
         }
 
         [Fact]
         public void Update_UpdateObject_Object()
         {
             // Arrange
-            var repository = new Mock<IRepository<MockObject>>();
+            var repository = new EFCoreRepository<UserEntity>(_appDbContext);
+
+            var user = new UserEntity()
+            {
+                Id = _users.First().Id,
+                Name = "TestName",
+            };
 
             // Act
-            var result = repository.Setup(u => u.Update(_object)).Returns(_object);
+            var result = repository.Update(user);
+            repository.Commit();
 
             // Assert
             Assert.NotNull(result);
-            Assert.Equal(_object, repository.Object.Update(_object));
+            Assert.Equal("TestName", result.Name);
+            Assert.NotEqual(_users.First(), result);
+            Assert.Equal(_users.First().Id, result.Id);
         }
-    }
 
-    public class MockObject
-    {
-        public Guid Id { get; set; }
-        public string Name { get; set; }
+        public void Dispose()
+        {
+            _appDbContext.Database.EnsureDeleted();
+            _appDbContext.Dispose();
+        }
     }
 }
