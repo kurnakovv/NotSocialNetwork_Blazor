c04fa72cbfb0365fd5e8f8261b6f74704b1d20fe
KurnakovMaksim <maksk210@gmail.com>
2021-05-05 13:51:40 +0900

    Add opportunity to use token in swagger

--- Src/Presentation/NotSocialNetwork.API/Startup.cs
+++ Src/Presentation/NotSocialNetwork.API/Startup.cs
@@ -27,31 +27,11 @@ namespace NotSocialNetwork.API
 
         public IConfiguration Configuration { get; }
 
-        private void ConfigureInMemoryDatabase(IServiceCollection services)
-        {
-            StartupSetup.AddMemoryDbContext(services, "MemoryDatabase");
-        }
-
-        private void ConfigureProductionServices(IServiceCollection services)
-        {
-            StartupSetup.AddDbContext(services, Configuration.GetConnectionString("NotSocialNetworkDB"));
-        }
-
-        private void ConnectMapping(IServiceCollection services)
-        {
-            var mapperConfig = new MapperConfiguration(mc =>
-            {
-                mc.AddProfile(new AutomapperConfig());
-            });
-
-            IMapper mapper = mapperConfig.CreateMapper();
-            services.AddSingleton(mapper);
-        }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.ConfigureServices();
+            services.AddControllers();
 
             // In-memory database.
             ConfigureInMemoryDatabase(services);
@@ -59,12 +39,7 @@ namespace NotSocialNetwork.API
             //ConfigureProductionServices(services);
 
             ConnectMapping(services);
-
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "NotSocialNetwork.API", Version = "v1" });
-            });
+            ConnectSwagger(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -88,6 +63,66 @@ namespace NotSocialNetwork.API
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+        }
+
+        private void ConfigureInMemoryDatabase(IServiceCollection services)
+        {
+            StartupSetup.AddMemoryDbContext(services, "MemoryDatabase");
+        }
+
+        private void ConfigureProductionServices(IServiceCollection services)
+        {
+            StartupSetup.AddDbContext(services, Configuration.GetConnectionString("NotSocialNetworkDB"));
+        }
+
+        private void ConnectMapping(IServiceCollection services)
+        {
+            var mapperConfig = new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new AutomapperConfig());
+            });
+
+            IMapper mapper = mapperConfig.CreateMapper();
+            services.AddSingleton(mapper);
+        }
+
+        private void ConnectSwagger(IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "NotSocialNetwork.API", Version = "v1" });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n" +
+                        "Enter 'Bearer' [space] and then your token in the text input below." +
+                        "\r\n\r\nExample: 'Bearer 12345abcdef'",
+
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer",
+                            },
+                            Scheme = "oatuh2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header,
+
+                        },
+                        new List<string>()
+                    }
+                });
             });
         }
     }
