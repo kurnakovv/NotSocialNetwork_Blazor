47678727d82ce6c88fdf9008cfa5acd3c9a9e46c
KurnakovMaksim <maksk210@gmail.com>
2021-05-19 17:04:16 +0900

    Add automapper in UserController

---
+++ Src/Core/NotSocialNetwork.Application/DTOs/UserDTO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace NotSocialNetwork.Application.DTOs
+{
+    public class UserDTO
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
