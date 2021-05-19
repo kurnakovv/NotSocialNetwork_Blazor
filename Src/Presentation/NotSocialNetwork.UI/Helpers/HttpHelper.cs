349822727baa63cca4535b769ab2e93d0aa81209
Ulyanov Ivan <ccoldatheinrich@yandex.ru>
2021-05-19 11:01:30 +0400

    Added handling of exceptions when sending HTTP requests.

---
+++ Src/Presentation/NotSocialNetwork.UI/Helpers/HttpHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NotSocialNetwork.UI.Helpers
+{
+    public class HttpHelper
+    {
+        internal const string APIAddress = "https://localhost:44353/api/";
+    }
+}
