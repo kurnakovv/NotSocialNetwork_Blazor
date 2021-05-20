56905427a4a7e38cd8f487f1da69397163ba48b1
Ulyanov Ivan <ccoldatheinrich@yandex.ru>
2021-05-20 12:53:11 +0400

    Switching theme now works.
    The styles for pages and components have been redesigned so that when the theme changes, they do not need to pass additional classes.
    
    Fixed an issue that resulted in a huge CPU load when the UI was running.

--- Src/Presentation/NotSocialNetwork.UI/Helpers/CssStyles.cs
+++ Src/Presentation/NotSocialNetwork.UI/Helpers/CssStyles.cs
@@ -4,10 +4,9 @@ namespace NotSocialNetwork.UI.Helpers
 {
     internal static class CssStyles
     {
-        internal const string DarkBackgroundClass = "dark-background";
-        internal const string DarkWindowClass = "dark-window";
+        internal const string DarkBackgroundClass = "dark-theme";
         internal const string DarkFooterClass = "dark-footer";
         internal const string DarkTitleClass = "dark-logo";
-        internal const string DarkButtonClass = "dark-button";
+        internal const string DisplayNone = "display-none";
     }
 }
