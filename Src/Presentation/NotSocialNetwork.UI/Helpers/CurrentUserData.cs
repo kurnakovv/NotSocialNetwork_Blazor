39f8a5b6687ed4edfc8bfa0344d3e9762523b92c
Ulyanov Ivan <ccoldatheinrich@yandex.ru>
2021-05-21 13:24:18 +0400

    СОздан функционал сохранения файлов, страница аккаунта корректно работает.
    Теперь на странице аккаунта загружаются посты только текущего пользователя (при условии что он аутентифицирован).

---
+++ Src/Presentation/NotSocialNetwork.UI/Helpers/CurrentUserData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NotSocialNetwork.UI.Helpers
+{
+    public static class CurrentUserData
+    {
+        public static Guid Id { get; set; }
+        public static string Token { get; set; }
+        public static string Email { get; set; }
+
+        public static bool IsNightTheme { get; set; }
+    }
+}
