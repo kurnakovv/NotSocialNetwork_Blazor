1f985c1d057e7d4243823ec51b100f49e8a17a65
KurnakovMaksim <maksk210@gmail.com>
2021-07-16 14:37:40 +0900

    Extract methods for check valid in User UseCase

--- Src/Core/NotSocialNetwork.Application/UseCases/User/GetUser.cs
+++ Src/Core/NotSocialNetwork.Application/UseCases/User/GetUser.cs
@@ -7,7 +7,6 @@ using NotSocialNetwork.Application.Interfaces.UseCases.User;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace NotSocialNetwork.Application.UseCases.User
 {
@@ -30,12 +29,9 @@ namespace NotSocialNetwork.Application.UseCases.User
         public UserEntity GetByEmail(string email)
         {
             var user = GetAll()
-                               .FirstOrDefault(u => u.Email == email);
+                           .FirstOrDefault(u => u.Email == email);
 
-            if (user == null)
-            {
-                throw new ObjectNotFoundException($"User by email: {email} not found.");
-            }
+            CheckUserValid(user, $"User by email: {email} not found.");
 
             return user;
         }
@@ -43,35 +39,54 @@ namespace NotSocialNetwork.Application.UseCases.User
         public UserEntity GetById(Guid id)
         {
             var user = GetAll()
-                            .FirstOrDefault(u => u.Id == id);
+                           .FirstOrDefault(u => u.Id == id);
 
-            if (user == null)
-            {
-                throw new ObjectNotFoundException($"User by Id: {id} not found.");
-            }
+            CheckUserValid(user, $"User by Id: {id} not found.");
 
             return user;
         }
 
         public IEnumerable<UserEntity> GetByPagination(int index)
         {
+            CheckIndexValid(index);
+            var users = GetUsers(index);
+            CheckUsersCountValid(users);
+
+            return users;
+        }
+
+        private void CheckUserValid(UserEntity user, string exceptionMessage)
+        {
+            if (user == null)
+            {
+                throw new ObjectNotFoundException(exceptionMessage);
+            }
+        }
+
+        private void CheckIndexValid(int index)
+        {
             if (IsInvalidIndex(index))
             {
                 throw new InvalidOperationException("Index cannot be less than 0.");
             }
+        }
 
+        private IEnumerable<UserEntity> GetUsers(int index)
+        {
             var countOfSkipItems = index * PaginationConfig.MAX_ITEMS;
-
             var users = GetAll()
                             .Skip(countOfSkipItems)
                             .Take(PaginationConfig.MAX_ITEMS);
 
+            return users;
+        }
+
+        private void CheckUsersCountValid(IEnumerable<UserEntity> users)
+        {
             if (IsEmptyUsersCount(users))
             {
                 throw new ObjectNotFoundException("No more users.");
             }
-
-            return users;
         }
 
         private bool IsInvalidIndex(int index)
