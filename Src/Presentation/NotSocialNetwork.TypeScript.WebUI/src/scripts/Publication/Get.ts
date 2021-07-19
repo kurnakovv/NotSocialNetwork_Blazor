b01bdd4920caa5e49f19f34cdc3a5e9f1640dcbe
KurnakovMaksim <maksk210@gmail.com>
2021-07-19 12:31:49 +0900

    Add main page for TS Client
    Now main page displays publications

---
+++ Src/Presentation/NotSocialNetwork.TypeScript.WebUI/src/scripts/Publication/Get.ts
@@ -0,0 +1,30 @@
+import { IPublication } from "./IPublication";
+
+async function GetPublications() {
+    const response = await fetch("https://localhost:5001/api/publication/index=0",{
+        method: "Get",
+        headers: { "Accept": "application/json" }
+    });
+
+    if(response.ok === true){
+        const publications = await response.json();
+        let rows = document.querySelector("tbody");
+        publications.forEach(publication => {
+            rows.append(setRow(publication));
+        });
+    }
+}
+
+function setRow(publication: IPublication): HTMLTableRowElement{
+    const tr = document.createElement("tr");
+
+    const authorNameTd = document.createElement("td");
+    authorNameTd.append(publication.author.name);
+    tr.append(authorNameTd);
+
+    const textTd = document.createElement("td");
+    textTd.append(publication.text);
+    tr.append(textTd);
+
+    return tr;
+}
---
+++ Src/Presentation/NotSocialNetwork.TypeScript.WebUI/src/scripts/Publication/IAuthor.ts
@@ -0,0 +1,4 @@
+export interface IAuthor{
+    id: string;
+    name: string;
+}
