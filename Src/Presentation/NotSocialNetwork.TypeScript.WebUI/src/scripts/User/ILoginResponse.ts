0c53554fb815d5eaa558e01a6179dc484cbaa1bb
KurnakovMaksim <maksk210@gmail.com>
2021-07-31 16:30:39 +0900

    Add basic login logic
    TS Client

---
+++ Src/Presentation/NotSocialNetwork.TypeScript.WebUI/src/scripts/User/ILoginResponse.ts
@@ -0,0 +1,4 @@
+export interface ILoginResponse {
+    token: string;
+    userId: string;
+}
---
+++ Src/Presentation/NotSocialNetwork.TypeScript.WebUI/src/scripts/User/Login.ts
@@ -0,0 +1,23 @@
+export async function Login(email: string) {
+    const response = await fetch("https://localhost:5001/api/authentication",{
+        method: "POST",
+        headers: { "Accept": "application/json", "Content-Type": "application/json" },
+        body: JSON.stringify({
+            email: email
+        })
+    });
+
+    if(response.ok === true){
+        const loginResponse = await response.json();
+        setDataInLocalStorage(loginResponse);
+
+        console.log("Welcome " + email + "!");
+    }else{
+        return console.log("Something went wrong...");
+    }
+}
+
+function setDataInLocalStorage(loginResponse){
+    localStorage.setItem("token", loginResponse.token);
+    localStorage.setItem("userId", loginResponse.userId);
+}
