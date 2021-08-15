b13b90d5ea67967c58c1bf7d75fd8fd5bcc10131
KurnakovMaksim <maksk210@gmail.com>
2021-08-15 08:31:02 +0300

    Add ability to click on like
    ReactTS

--- Src/Presentation/NotSocialNetwork.ReactTS.Client/notsocialnetwork-react-ts-client/src/components/Publication/ShortPublication.tsx
+++ Src/Presentation/NotSocialNetwork.ReactTS.Client/notsocialnetwork-react-ts-client/src/components/Publication/ShortPublication.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ShortPublication.css"
 import UnfavoriteImg from "../../img/unfavorite.png";
 import FavoriteImg from "../../img/favorite.png";
@@ -6,7 +6,32 @@ import CommentsImg from "../../img/comments.png";
 import BookmarkImg from "../../img/bookmark.png";
 import Button from "../Button";
 
-export class ShortPublication extends React.Component {
+
+interface IShortPublicationState {
+    isFavorite: boolean;
+    favoriteImage: string;
+}
+
+export class ShortPublication extends React.Component<any, IShortPublicationState> {
+    constructor(props: any){
+        super(props);
+        this.state = {
+            isFavorite: false,
+            favoriteImage: UnfavoriteImg,
+        }
+        this.setIsFavorite = this.setIsFavorite.bind(this);
+    }
+
+    setIsFavorite(): void{
+        this.setState({isFavorite: !this.state.isFavorite});
+
+        if(this.state.isFavorite){
+            this.setState({favoriteImage: FavoriteImg});
+        } else {
+            this.setState({favoriteImage: UnfavoriteImg});
+        }
+    }
+
     render() {
         return (
             <>
@@ -18,7 +43,7 @@ export class ShortPublication extends React.Component {
                     <img className="MainImage" src="https://analyticsindiamag.com/wp-content/uploads/2020/10/7d744a684fe03ebc7e8de545f97739dd.jpg"/>
                     <div className="Panel">
                         <ul className="Panel">
-                            <li className="RightButton"><img width={30} height={30} src={UnfavoriteImg}/></li>
+                            <li className="RightButton"><img width={30} height={30} onClick={this.setIsFavorite} src={this.state.favoriteImage}/></li>
                             <li className="RightButton"><img width={30} height={30} src={CommentsImg} /></li>
                             <li className="RightButton"><img width={30} height={30} src={BookmarkImg} /></li>
                             <li className="LeftButton"><Button text="More" reference="/"/></li>
