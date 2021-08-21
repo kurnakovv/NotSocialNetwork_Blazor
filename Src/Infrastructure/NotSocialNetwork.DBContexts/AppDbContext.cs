5756bc4586a4211f7212de79712cd63d95e771aa
KurnakovMaksim <maksk210@gmail.com>
2021-08-21 20:49:55 +0900

    Add rules for entities

--- Src/Infrastructure/NotSocialNetwork.DBContexts/AppDbContext.cs
+++ Src/Infrastructure/NotSocialNetwork.DBContexts/AppDbContext.cs
@@ -10,5 +10,38 @@ namespace NotSocialNetwork.DBContexts
         public DbSet<UserEntity> Users { get; set; }
         public DbSet<PublicationEntity> Publications { get; set; }
         public DbSet<ImageEntity> Images { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserEntity>(user =>
+            {
+                user.Property(u => u.Name)
+                    .IsRequired()
+                    .HasMaxLength(30);
+
+                user.Property(u => u.Email)
+                    .IsRequired();
+
+                user.Property(u => u.DateOfBirth)
+                    .IsRequired();
+
+                user.Property(u => u.Role)
+                    .IsRequired();
+            });
+
+            modelBuilder.Entity<PublicationEntity>(publication =>
+            {
+                publication.Property(p => p.Text)
+                    .IsRequired()
+                    .HasMaxLength(100);
+            });
+
+            modelBuilder.Entity<ImageEntity>(image =>
+            {
+                image.Property(i => i.Title)
+                    .IsRequired()
+                    .HasMaxLength(100);
+            });
+        }
     }
 }
