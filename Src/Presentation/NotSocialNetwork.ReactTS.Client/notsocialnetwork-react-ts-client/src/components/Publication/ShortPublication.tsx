4aaeda9511d4140e321eed079d2e00d51a063688
KurnakovMaksim <maksk210@gmail.com>
2021-09-11 13:30:20 +0900

    Add border-radius for publication
    ReactTS

--- Src/Presentation/NotSocialNetwork.ReactTS.Client/notsocialnetwork-react-ts-client/src/components/Publication/ShortPublication.tsx
+++ Src/Presentation/NotSocialNetwork.ReactTS.Client/notsocialnetwork-react-ts-client/src/components/Publication/ShortPublication.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "./ShortPublication.css"
 import UnfavoriteImg from "../../img/unfavorite.png";
 import FavoriteImg from "../../img/favorite.png";
