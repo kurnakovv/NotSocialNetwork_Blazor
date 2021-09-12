76f5e667d395711ad35c2f043da6c9ed0015b2de
KurnakovMaksim <maksk210@gmail.com>
2021-09-12 20:18:00 +0900

    Add button for close eventWindow
    ReactTS

--- Src/Presentation/NotSocialNetwork.ReactTS.Client/notsocialnetwork-react-ts-client/src/components/EventWindow/index.tsx
+++ Src/Presentation/NotSocialNetwork.ReactTS.Client/notsocialnetwork-react-ts-client/src/components/EventWindow/index.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 
 import "../EventWindow/EventWindow.css";
-import Button from "../Button";
+import "../Button/Button.css";
 import AppContext from "../../contexts/AppContext";
 
 const EventWindow: React.FC = ({}) => {
 
     const context = React.useContext(AppContext);
-    const { eventWindowIsVisible, eventWindowText, eventWindowImg } = context;
+    const { eventWindowIsVisible, setEventWindowIsVisible, eventWindowText, eventWindowImg } = context;
 
     return <>
         <div className="EventWindow" style={{display: eventWindowIsVisible ? "" : "none"}}>
-            <div className="ExitButton"><Button reference="/" text="X" /></div>
+            <div className="ExitButton"><button className="Button" onClick={() => setEventWindowIsVisible(false)}>X</button></div>
             <p>{eventWindowText}</p>
             <img className="WindowImg" width={25} height={25} src={eventWindowImg} />
         </div>
